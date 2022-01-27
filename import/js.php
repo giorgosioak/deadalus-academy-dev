4987133b80894cecba4aa86756b2ec282ed5b46d
George Ioakeimidis <giorgosioak95@gmail.com>
2022-01-27 19:51:30 +0200

    less css and js includes

--- import/js.php
+++ import/js.php
@@ -1,34 +1,8 @@
   <!-- Semantic UI -->
   <script src="/js/jquery-3.1.1.min.js"></script>
   <script src="/js/jquery.validate.min.js"></script>
-  <script src="/semantic/semantic.js"></script>
-
-  <!-- Components -->
-  <!-- <script src="/semantic/components/accordion.js"></script>
-  <script src="/semantic/components/api.js"></script>
-  <script src="/semantic/components/checkbox.js"></script>
-  <script src="/semantic/components/colorize.js"></script>
-  <script src="/semantic/components/dimmer.js"></script>
-  <script src="/semantic/components/dropdown.js"></script>
-  <script src="/semantic/components/embed.js"></script>
-  <script src="/semantic/components/form.js"></script>
-  <script src="/semantic/components/modal.js"></script>
-  <script src="/semantic/components/nag.js"></script>
-  <script src="/semantic/components/popup.js"></script>
-  <script src="/semantic/components/progress.js"></script>
-  <script src="/semantic/components/rating.js"></script>
-  <script src="/semantic/components/search.js"></script>
-  <script src="/semantic/components/shape.js"></script>
-  <script src="/semantic/components/sidebar.js"></script>
-  <script src="/semantic/components/site.js"></script>
-  <script src="/semantic/components/state.js"></script>
-  <script src="/semantic/components/sticky.js"></script>
-  <script src="/semantic/components/tab.js"></script>
-  <script src="/semantic/components/transition.js"></script>
-  <script src="/semantic/components/video.js"></script>
-  <script src="/semantic/components/visibility.js"></script>
-  <script src="/semantic/components/visit.js"></script> -->
-
+  <script src="/semantic/semantic.min.js"></script>
+  
   <script>
     $('.ui.checkbox')
       .checkbox();
